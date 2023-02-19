8a4c5c1a7e1433267c456a92c798a52aefe8cddb
MorganStefanie <109448525+MorganStefanie@users.noreply.github.com>
2023-02-19 04:16:29 -0600

    Add files via upload

---
+++ Gus's Restaurant.java
@@ -0,0 +1,21 @@
+public class Main {
+    /**
+     * Gus's Restaurant
+     */
+    public interface Gus {
+     string();
+     main method();
+
+    }
+    public class Reservation {
+    /**
+     * Number of customer's waiting
+     */
+    
+
+    }
+}
+
+public class Lunch {
+    
+}
