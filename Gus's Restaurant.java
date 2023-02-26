4bc851de32d60a6117e518b1b421fe328d27b78a
MorganStefanie <109448525+MorganStefanie@users.noreply.github.com>
2023-02-26 01:26:20 -0600

    Update Gus's Restaurant.java

--- Gus's Restaurant.java
+++ Gus's Restaurant.java
@@ -1,21 +1,39 @@
-public class Main {
-    /**
-     * Gus's Restaurant
-     */
-    public interface Gus {
-     string();
-     main method();
-
-    }
-    public class Reservation {
-    /**
-     * Number of customer's waiting
-     */
-    
-
-    }
+@SpringBootApplication
+@RestController
+import java.util 
+    public static class Food Managment {
+    int "CEO";
+    add_employee char "vanessa williams",
+    char "vick hammerhead",
+    char "jesse bill",
+    char "phillip point",
+    system.println(employees);
+    [createrecord ("Line Manager")char "vanessa"];
+    [updaterecord ("CEO")char "vick"];
+    [readrecord ("Store Manager")char "jesse"];
+    [deleterecord ("Assistant Manager")char "phillip"];
+    system.println()
 }
-
-public class Lunch {
-    
-}
+    public class Front Service extends Food Management{
+    int "waiter/waitress";
+    add_employee char "vanessa williams",
+    char "vick hammerhead",
+    char "jesse bill",
+    char "phillip point",
+    delete_employee 
+    system.println(front);
+} 
+    public class Table Service extends Front Service{
+    int "reservation";
+    **/
+    * table one seats two people
+    **/
+    * table two seats three people
+    **/
+    * table three seats twenty people
+    **/
+    system.println(reservation)
+    system.println(today menu)
+    system.println(date)
+    system.println(time)
+    }
